9a889cb8e445a00c85cf9529453821afa0345b2d
Nancy Hamdan <nancyehamdan@gmail.com>
2024-01-10 13:38:30 +0300

    frontend: displaying backend messages, display keep going feedback, handle wrong cs

--- frontend/src/components/FeedbackDisplay.jsx
+++ frontend/src/components/FeedbackDisplay.jsx
@@ -1,13 +1,38 @@
 import PropTypes from 'prop-types';
 import { Box, Card, CardContent, Typography, CircularProgress } from '@mui/material';
+import { green } from '@mui/material/colors'
 
-const FeedbackDisplay = ({ currentFeedbackMessages, feedbackMessages, isExerciseFinished, clinicalScore }) => {
+const FeedbackDisplay = ({ isSaving, currentFeedbackMessages, feedbackMessages, isExerciseFinished, clinicalScore }) => {
     return (
         <Box
           width="80%"
           height="100%"
           overflow="hidden"
         >
+            {!isExerciseFinished && isSaving && currentFeedbackMessages.length === 0 && (
+                <Box 
+                    raised
+                    component={Card} 
+                    mx={1} 
+                    flex="0 0 auto"
+                    display="flex" 
+                    justifyContent="center" 
+                    width="100%"
+                    sx={{ bgcolor: green[500] }}
+                >
+                    <CardContent>
+                        <Typography 
+                            color="textSecondary" 
+                            gutterBottom 
+                            variant="h5"
+                            align="center"
+                            sx={{ color: '#fff' }}
+                        >
+                            Keep going!
+                        </Typography>
+                    </CardContent>
+                </Box>
+            )}
             {!isExerciseFinished && (
                 <Box 
                 display="flex" 
@@ -18,8 +43,8 @@ const FeedbackDisplay = ({ currentFeedbackMessages, feedbackMessages, isExercise
                 >
                     {currentFeedbackMessages.map((m, i) => (
                         <Box 
-                        raised
-                        key={i} 
+                            raised
+                            key={i} 
                         component={Card} 
                         mx={1} 
                         flex="1 0 auto"
@@ -39,36 +64,48 @@ const FeedbackDisplay = ({ currentFeedbackMessages, feedbackMessages, isExercise
             )}
             {isExerciseFinished &&
                 <Box style={{marginTop: 0}}>
-                <Typography variant="h6" component="div" style={{ textAlign: "center" }}>
-                    Your Predicted Clinical Score is {clinicalScore !== null ? clinicalScore : <CircularProgress />}
-                </Typography>
-                <Box 
-                display="flex" 
-                flexDirection="row" 
-                overflow="auto" 
-                width="100%"
-                p={2}
-                >
-                    {feedbackMessages.map((m, i) => (
-                        <Box
-                        raised
-                        key={i} 
-                        component={Card} 
-                        mx={1} 
-                        flex="1 0 auto"
-                        minWidth={250}
-                        >
-                            <CardContent>
-                                <Typography color="textSecondary" gutterBottom variant="h5">
-                                    Feedback {i+1}
-                                </Typography>
-                                <Typography variant="h5" component="p">
-                                    {m.message}
-                                </Typography>
-                            </CardContent>
-                        </Box>
-                    ))}
-                </Box>
+                {clinicalScore === null ? (
+                    <Typography variant="h6" component="div" align="center">
+                        Finding clinical score... <CircularProgress size={20} />
+                    </Typography>
+                ) : (
+                    clinicalScore > 50 || clinicalScore < 0 ? (
+                        <Typography variant="h6" component="div" align="center">
+                            It appears that you had too many mistakes in your exercise. Please try again to get a clinical score.
+                        </Typography>
+                    ) : (
+                        <Typography variant="h6" component="div" align="center">
+                            Clinical Score: {clinicalScore.toFixed(2)}
+                        </Typography>
+                        )
+                )}
+                    <Box 
+                    display="flex" 
+                    flexDirection="row" 
+                    overflow="auto" 
+                    width="100%"
+                    p={2}
+                    >
+                        {feedbackMessages.map((m, i) => (
+                            <Box
+                            raised
+                            key={i} 
+                            component={Card} 
+                            mx={1} 
+                            flex="1 0 auto"
+                            minWidth={250}
+                            >
+                                <CardContent>
+                                    <Typography color="textSecondary" gutterBottom variant="h5">
+                                        Feedback {i+1}
+                                    </Typography>
+                                    <Typography variant="h5" component="p">
+                                        {m.message}
+                                    </Typography>
+                                </CardContent>
+                            </Box>
+                        ))}
+                    </Box>
                 </Box>
             }
         </Box>
@@ -76,6 +113,7 @@ const FeedbackDisplay = ({ currentFeedbackMessages, feedbackMessages, isExercise
 };
 
 FeedbackDisplay.propTypes = {
+isSaving: PropTypes.bool,
     currentFeedbackMessages: PropTypes.array.isRequired,
     feedbackMessages: PropTypes.array.isRequired,
     isExerciseFinished: PropTypes.bool.isRequired,
